6bf4723939c8bc9a7e5859b7dc40565a89e8d6a5
nadinedelia <nadine.delia@yahoo.com>
2020-08-28 00:25:27 +0200

    deposit function

--- src/account.java
+++ src/account.java
@@ -5,4 +5,14 @@ public class Account {
         this.name = name;
         balance = 0.0;
     }
+    public double getBalance() {
+        return balance;
+    }
+    public String getName() {
+        return name;
+    }
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println(name + " has £" + balance);
+    }
 }
