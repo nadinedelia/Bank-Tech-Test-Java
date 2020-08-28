d4aa2d2ac456bdf3dbb8f4be639a821ed2c92664
nadinedelia <nadine.delia@yahoo.com>
2020-08-28 09:27:43 +0200

    account 1 to account 2 transfer, withdraw account 2

--- src/Driver.java
+++ src/Driver.java
@@ -5,5 +5,7 @@ public class Driver {
         nadine.deposit(500.0);
         Account ejila = new Account("Ejila");
         ejila.deposit(200.0);
+        nadine.transfer(100.0, ejila);
+        ejila.withdraw(50.0);
     }
 }
