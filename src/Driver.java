471f975901703014ae291875e5be570f2b0baabd
nadinedelia <nadine.delia@yahoo.com>
2020-08-28 09:25:59 +0200

    setting up 2 accounts with deposit

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,9 @@
 public class Driver {
 
+    public static void main(String[] args) {
+        Account nadine = new Account("Nadine");
+        nadine.deposit(500.0);
+        Account ejila = new Account("Ejila");
+        ejila.deposit(200.0);
+    }
 }
